3b6fb6dd2f433cf6b45aac39ef08172c94dd0621
caojunzhi <caojunzhi@10-17-246-67.dynapool.wireless.nyu.edu>
2019-06-05 13:30:16 -0400

    C code for reading array

---
+++ main.c
@@ -0,0 +1,19 @@
+//
+//  main.c
+//  read_hdf5
+//
+//  Created by caojunzhi on 6/5/19.
+//  Copyright © 2019 caojunzhi. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    int count = 1000;
+    double balance[count];
+    int i;
+    
+    for(i=0;i<count;i++)
+        scanf("%lf",&balance[i]);
+}
